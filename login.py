79159ef56c0d253387cd03dd6f1bec93501739b1
planksconstant <abhishekdharanisha@icloud.com>
2025-11-04 20:24:31 +0530

    added web drive version as mentioned in documentation

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By  # Add this import
 import time
 def read():
     a=open("credential.txt","r")
@@ -7,6 +8,11 @@ def read():
     return credentials_list
 def automator():
     driver = webdriver.Safari()
-    a=driver.get("http://www.python.org")
-    print(type(a),a)
-automator()l
+    driver.get("http://192.168.254.1:8090/httpclient.html")
+    time.sleep(0.5)
+    driver.find_element(By.NAME, "username").send_keys(username)
+    driver.find_element(By.NAME, "password").send_keys(password)
+    driver.find_element(By.ID, "loginbutton").click()
+    time.sleep(0.5)
+
+automator()
