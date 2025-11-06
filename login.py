dcef1419cc55fbe4a45cf05f04aadb4a902e370c
planksconstant <abhishekdharanisha@icloud.com>
2025-11-06 15:18:17 +0530

    Made changes for entering elements into the field of captive

--- login.py
+++ login.py
@@ -3,13 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By  # Add this import
 import time
 def read():
-    a=open("credential.txt","r")
+    a=open("credentials.txt","r")
     credentials_list=a.readlines()
     return credentials_list
 def automator():
+    x=read()
     driver = webdriver.Safari()#change it to chrome or some other browser while running on windows or linux
     #fix required here
     driver.get("http://192.168.254.1:8090/httpclient.html")
+    username=x[0]
+    password=x[1]
     time.sleep(0.5)
     driver.find_element(By.NAME, "username").send_keys(username)
     driver.find_element(By.NAME, "password").send_keys(password)
