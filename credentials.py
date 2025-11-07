09252c032d8dd8f8772ebcf698049276b9475b15
planksconstant <abhishekdharanisha@icloud.com>
2025-11-07 16:25:06 +0530

    hard coded to make it OS/browser independent

--- credentials.py
+++ credentials.py
@@ -5,34 +5,6 @@ import stdiomask
 platform = platform.system()
 
 class login():
-    def config(self,l):
-        print(l)
-        if l=="allow":
-
-
-            a = open("credentials.txt", "a")
-            if platform=="Darwin":#Darwin is mac
-                print("Looks like you are on Mac OS ")
-                print("Are you on an ARM processor? (Apple M Series Chip)")
-                if input("[y/n]") == "y":
-                    a.write("chromedriver_macos_arm")
-                else:
-                    a.write("chromedriver_macos_x64")
-            elif platform=="Linux":
-                print("Looks like you are on Linux")
-                a.write("chromedriver_linux_64")
-            elif platform=="Windows":
-                print("Looks like you are on Windows")
-                a.write("chromedriver_win64.exe")
-            else:
-                print("Looks like you are on an unsupported platform :( ")
-        else:
-            print("Configuration Issue Contact @planksconstant ")
-
-
-
-
-
     def accept_name_password(self):
         x=""
         if os.path.exists('credentials.txt'):
@@ -48,7 +20,6 @@ class login():
                 a.write(srn + "\n")
                 # a.write("\n")
                 a.write(pwd + "\n")
-
         else:
             a = open("credentials.txt", "w")
             srn=input("Enter SRN : ")
@@ -64,28 +35,23 @@ class login():
         l=file.readlines()
         srn=l[0]
         pwd=l[1]
-        c=l[2]
-        print(srn)
-        print(l)
+        #c=l[2]-->driver name
         a=input("Enter Your SRN ")
-        if (a+'\n')!=srn:
+        print(srn)
+        if (a)!=srn:
             print("SRN entered is wrong")
         else:
             b= stdiomask.getpass("Password: ", mask='*')
-            if (b+'\n')!=pwd:
+            if (b)!=pwd:
                 print("Password entered is wrong ")
             else:
                 srn=input("Enter New SRN : ")
                 pwd= stdiomask.getpass("Password: ", mask='*')
-                l=[srn,pwd,c]
-
+                l=[srn,pwd]
         file.close()
         file=open("credentials.txt", "w")
         for j in l:
             file.write(j+"\n")
-
-
-
 n=0
 while n<3:
     print("Enter the numbers for the following operations")
@@ -94,8 +60,6 @@ while n<3:
     print("3=Exit the menu")
     n=int(input(""))
     if n==1:
-        #a=login().accept_name_password()
-        #login().config(a)
         login().accept_name_password()
     elif n==2:
         login().change_password()
@@ -103,5 +67,3 @@ while n<3:
         break
     else:
         break
-
-
--- login.py
+++ login.py
@@ -1,22 +1,39 @@
 import selenium
+import platform
 from selenium import webdriver
 from selenium.webdriver.common.by import By  # Add this import
 import time
+def setup_drive():
+    platform_system = platform.system()
+    if platform_system == "Darwin":  # Darwin is mac
+        print("Looks like you are on Mac OS ")
+        return webdriver.Safari()
+    elif platform_system == "Linux":
+        print("Looks like you are on Linux")
+        return webdriver.Chrome(service=service)
+    elif platform_system=="Windows":
+        print("Looks like you are on windows")
+        return webdriver.Edge()
+    else:
+        print("Unsuported OS :(")
 def read():
     a=open("credentials.txt","r")
     credentials_list=a.readlines()
     return credentials_list
 def automator():
     x=read()
-    driver = webdriver.Safari()#change it to chrome or some other browser while running on windows or linux
-    #fix required here
+    driver = setup_drive()
+    username = x[0]
+    password = x[1]
     driver.get("http://192.168.254.1:8090/httpclient.html")
-    username=x[0]
-    password=x[1]
-    time.sleep(0.5)
+    time.sleep(0.5)#required to make sure the fields do not get bad text
     driver.find_element(By.NAME, "username").send_keys(username)
     driver.find_element(By.NAME, "password").send_keys(password)
     driver.find_element(By.ID, "loginbutton").click()
     time.sleep(0.5)
+    driver.quit()
 
 automator()
+
+#login failed ==> <selenium.webdriver.remote.webelement.WebElement (session="9F63AA64-D1DE-45AA-B979-E37440C374B3", element="node-7AF1754A-1D40-44D0-B696-DA020798E7DF")>
+#login success==> <selenium.webdriver.remote.webelement.WebElement (session="F821B13F-A9BC-4EDC-A196-2D2D8370E7DF", element="node-CCEE84C6-F0E9-417E-97E6-91615926F273")>
